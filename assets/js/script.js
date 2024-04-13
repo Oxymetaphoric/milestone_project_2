21e549078810fe75bb6dc633a95fd55c40566146
Oxymetaphoric <140067668+Oxymetaphoric@users.noreply.github.com>
2024-04-13 17:20:53 +0100

    build: continue building out fetchCards method

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,32 @@
 const apiLink = "https://api-preview.netrunnerdb.com/api/v3/public/"
 
 
-async function fetchCards(link, filter, param) {
+async function fetchCards(link, filter, param, page = 1) {
   const response = await fetch(`${link}cards?filter[${filter}]=${param}`);
   const data = await response.json();
   return data.data;
   }
 
-async function getAndLogCards() {
+async function getAllCards() {
   let allCards = await fetchCards(apiLink, "side_id", "runner");
   return allCards
   }
   
 //filter returned cards
 //TODO replace hardcoded values with passed values 
-async function extractIDs() {
-  let allCards = await getAndLogCards();
-  let filteredCards = allCards.filter(card => card.attributes.card_type_id === 'runner_identity').map(card => {
-  return $("<div>").html("<h1>" + card.attributes.title + "</h1>");("#allCards");
+async function filterCards(cardProperty) {
+  let allCards = await getAllCards();
+  let filteredCards = allCards.filter(card => card.attributes[cardProperty] === 'runner_identity').map(card => {
+   return $("<div>").html("<h1>" + card.attributes.title + "</h1>");("#allCards");
   }
 );
       $("#allCards").append(filteredCards);
-  }
+    }
     
   
 
 
-extractIDs();
-
+filterCards("card_type_id");
 
 
 
