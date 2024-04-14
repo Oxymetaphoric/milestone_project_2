fabb41789bf5f55497adeab583ed4bad52b9108c
Oxymetaphoric <140067668+Oxymetaphoric@users.noreply.github.com>
2024-04-14 02:24:36 +0100

    build: tidy main()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,26 @@
 const apiLink = "https://api-preview.netrunnerdb.com/api/v3/public/"
-//let userSide = "corp"
+let userSide = ""
 
 
 //connect to netrunnerdb and fetch cards with a url crafted from arguments
-async function fetchCards(apiLink, filter = "", param = "") {
-  const response = await fetch(`${apiLink}${filter}${param}`);
+async function fetchCards(apiLink, filter = "", param = "", side) {
+  const response = await fetch(`${apiLink}${filter}?filter[${param}]=${side}`);
   const data = await response.json();
   let next = data.links.next;
   let prev = data.links.prev;
-
 //call function recursively, while data.links.next has a truthy value, i.e until there are no more pages
   if (next) {
     const nextData = await fetchCards(next);
     data.data = [...data.data, ...nextData];
   }
-//return a concatenated array of card objects 
-
+//return an array of card objects 
 return data.data;
   }
 
 //filter requested cards
 //TODO replace hardcoded values with passed values 
 async function filterCards(cardProperty = "", cardFilter = "", side = "") {
-  let allCards = await fetchCards(apiLink, "cards?filter[side_id]=", side);
+  let allCards = await fetchCards(apiLink, "cards", "side_id", side);
   let filteredCards = allCards.filter(card => card.attributes[cardProperty] === cardFilter).map(card => {
     return $("<div>").html("<h1>" + card.attributes.title + "</h1>");("#allCards");
     }
@@ -41,12 +39,15 @@ async function getCardTypes(side) {
     return availableTypes
     }  
 
+
 //Main function, for calling other functions
-async function main() {
-userSide = "runner" //runner or corp
-cardType = "runner_identity" //type of card see console.log for available choices per side
-let userCardTypes = await getCardTypes(userSide);
-filterCards("card_type_id", cardType, userSide);
+async function main(side) {
+side = "runner" //runner or corp
+let userCardTypes = await getCardTypes(side);
+cardType = userCardTypes[4].id //see console.log for available choices filtered by side
+filterCards("card_type_id", cardType, side);
 }
 
-main();
+main(userSide);
+
+//let cardTypes = userCardTypes.filter(type => type.attributes.side_id === type)
